042194528f57d164d56f99666be7b022e3661d8c
unknown <teranisi@cc.it-hiroshima.ac.jp>
2020-06-28 17:14:39 +0900

    doom themeとpomodoro

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -42,22 +42,6 @@
                 ;; org-mode
                 ("org"   . "http://orgmode.org/elpa/")
                 ))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(doom-themes-enable-bold t)
- '(doom-themes-enable-italic t)
- '(package-selected-packages (quote (org-pomodoro package-utils use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(doom-modeline-bar ((t (:background "#6272a4"))))
- '(org-pomodoro-mode-line ((t (:foreground "#ff5555"))))
- '(org-pomodoro-mode-line-break ((t (:foreground "#50fa7b")))))
 
 
 ;;;#direct settings
@@ -71,46 +55,116 @@
 ;; 行間を指定
 (setq-default line-spacing 0.2)
 
+;; 表示を単純化（スタートアップメッセージなし．スクラッチは空．ツールバー無し，
+;; スクロールバーなし）．，ベル無し．
+(setq ring-bell-function 'ignore)
+(setq inhibit-startup-message t)
+(setq initial-scratch-message "") 
+(tool-bar-mode -1)
+(set-scroll-bar-mode nil)
+(setq frame-title-format (format "%%f - Emacs@%s" (system-name)))
+
+
+;;;# for org-mode
+  ;; TODO状態
+  (setq org-todo-keywords
+        '((sequence "APPT(a@/!)" "TODO(t)" "STARTED(s!)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCEL(c@/!)" "SOMEDAY(s@/!)"))
+        )
+  ;; DONEの時刻を記録
+  (setq org-log-done 'time)
+  ;; アジェンダ表示の対象ファイル
+  (setq org-agenda-files '(
+                           "~/org/gtd.org"
+                ;;           "~/org/active"
+                           )
+        )
+  ;; refile target
+  (setq org-refile-targets '((org-agenda-files :maxlevel . 3)))
+  ;; アジェンダ表示で下線を用いる
+  (add-hook 'org-agenda-mode-hook '(lambda () (hl-line-mode 1)))
+  (setq hl-line-face 'underline)
+  ;; 標準の祝日を利用しない
+  (setq calendar-holidays nil)
+  ;; org-capture 構成
+ (setq org-capture-templates
+       '(("t" "Todo" entry (file+headline "~/org/gtd.org" "Inbox")
+          "* TODO %? (wrote on %U)")
+         ("k" "Knowledge" entry (file+headline "~/org/knowledge.org" "Inbox")
+          "* %?  # Wrote on %U")
+         )
+ )
+
 
 ;;;#settings by packages
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;#all-the-icons
+(use-package all-the-icons
+  :ensure t
+  )
+
+;;;#emoji (for the following themes)
+(use-package emojify :ensure t
+  :if (display-graphic-p)
+  :hook (after-init . global-emojify-mode)
+  :bind
+  ("C-x e" . 'emojify-insert-emoji)
+  )
+
 ;;;#theme
-;;; doom theme
+;;;# doom theme
 (use-package doom-themes
     :ensure t
     :custom
-    (doom-themes-enable-italic t)
-    (doom-themes-enable-bold t)
-    :custom-face
-    (doom-modeline-bar ((t (:background "#6272a4"))))
-    :config
-    (load-theme 'doom-dracula t)
+     (doom-themes-enable-italic t)
+     (doom-themes-enable-bold t)
+     :custom-face
+     (doom-modeline-bar ((t (:background "#6272a4"))))
+      :config
+      (load-theme 'doom-dracula t)
     (doom-themes-neotree-config)
-    (doom-themes-org-config))
+    (doom-themes-org-config)
+    )
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;#org-pomodoro
+;;;#task-measuring
+(defun ladicle/task-clocked-time ()
+        "Return a string with the clocked time and effort, if any"
+        (interactive)
+        (let* ((clocked-time (org-clock-get-clocked-time))
+               (h (truncate clocked-time 60))
+               (m (mod clocked-time 60))
+               (work-done-str (format "%d:%02d" h m)))
+          (if org-clock-effort
+              (let* ((effort-in-minutes
+                  (org-duration-to-minutes org-clock-effort))
+                 (effort-h (truncate effort-in-minutes 60))
+                 (effort-m (truncate (mod effort-in-minutes 60)))
+                 (effort-str (format "%d:%02d" effort-h effort-m)))
+            (format "%s/%s" work-done-str effort-str))
+            (format "%s" work-done-str))))
+;;;#pomodoro
 (use-package org-pomodoro
-  :ensure t
     :after org-agenda
     :custom
     (org-pomodoro-ask-upon-killing t)
-    (org-pomodoro-format "%s")
-    (org-pomodoro-short-break-format "%s")
-    (org-pomodoro-long-break-format  "%s")
+    (org-pomodoro-format "%s")
+    (org-pomodoro-short-break-format "%s")
+    (org-pomodoro-long-brea-format  "%s")
     :custom-face
     (org-pomodoro-mode-line ((t (:foreground "#ff5555"))))
     (org-pomodoro-mode-line-break   ((t (:foreground "#50fa7b"))))
     :hook
-    (org-pomodoro-started . (lambda () (notifications-notify
-                                               :title "org-pomodoro"
-                           :body "Let's focus for 25 minutes!"
-                           :app-icon "~/.emacs.d/img/001-food-and-restaurant.png")))
-    (org-pomodoro-finished . (lambda () (notifications-notify
-                                               :title "org-pomodoro"
-                           :body "Well done! Take a break."
-                           :app-icon "~/.emacs.d/img/004-beer.png")))
+    ;; (org-pomodoro-started . (lambda () (notifications-notify
+    ;;                                            :title "org-pomodoro"
+    ;;                        :body "Let's focus for 25 minutes!"
+    ;; 			   ;;                           :app-icon "~/.emacs.d/img/001-food-and-restaurant.png"
+    ;; 			   )))
+    ;; (org-pomodoro-finished . (lambda () (notifications-notify
+    ;;                                            :title "org-pomodoro"
+    ;;                        :body "Well done! Take a break."
+    ;; 			   ;;                           :app-icon "~/.emacs.d/img/004-beer.png"
+    ;; 			   )))
     :config
     :bind (:map org-agenda-mode-map
                 ("p" . org-pomodoro)))
+
